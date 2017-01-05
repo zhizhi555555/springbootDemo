8337311841e22ff8c4361a597d98b01613ce7b5a
zhuxuguang555@163.com <zhuxuguang555@163.com>
2017-01-05 21:04:28 +0800

    使用Thymeleaf模板引擎渲染web视图

--- src/main/java/melon/web/HelloController.java
+++ src/main/java/melon/web/HelloController.java
@@ -1,12 +1,30 @@
 package melon.web;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-@RestController
+/**
+ *
+ * @author 程序猿DD
+ * @version 1.0.0
+ * @blog http://blog.didispace.com
+ *
+ */
+@Controller
 public class HelloController {
-	@RequestMapping("/hello")
-	public String index() {
-		return "Hello World";
-	}
+    
+    @ResponseBody
+    @RequestMapping("/hello")
+    public String hello() {
+        return "Hello World";
+    }
+
+    @RequestMapping("/")
+    public String index(ModelMap map) {
+        map.addAttribute("host", "光哥你好厉害");
+        return "index";
+    }
+
 }
--- src/main/test/melon/ApplicationTest.java
+++ src/main/test/melon/ApplicationTest.java
@@ -1,41 +1,49 @@
 package melon;
 
+import melon.web.HelloController;
 
-
-import melon.service.BlogProperties;
-
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockServletContext;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+/**
+ *
+ * @author 程序猿DD
+ * @version 1.0.0
+ * @blog http://blog.didispace.com
+ *
+ */
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(FirstApplication.class)
+@SpringApplicationConfiguration(classes = MockServletContext.class)
+@WebAppConfiguration
 public class ApplicationTest {
 
-	private static final Log log = LogFactory.getLog(ApplicationTest.class);
+	private MockMvc mvc;
 
-	@Autowired
-	private BlogProperties blogProperties;
-
+	@Before
+	public void setUp() throws Exception {
+		mvc = MockMvcBuilders.standaloneSetup(
+				new HelloController()).build();
+	}
 
 	@Test
-	public void test1() throws Exception {
-		Assert.assertEquals("程序猿DD", blogProperties.getName());
-		Assert.assertEquals("Spring Boot教程", blogProperties.getTitle());
-		Assert.assertEquals("程序猿DD正在努力写《Spring Boot教程》", blogProperties.getDesc());
-
-		log.info("随机数测试输出：");
-		log.info("随机字符串 : " + blogProperties.getValue());
-		log.info("随机int : " + blogProperties.getNumber());
-		log.info("随机long : " + blogProperties.getBignumber());
-		log.info("随机10以下 : " + blogProperties.getTest1());
-		log.info("随机10-20 : " + blogProperties.getTest2());
-
+	public void getHello() throws Exception {
+		mvc.perform(MockMvcRequestBuilders.get("/hello").accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk())
+				.andExpect(content().string(equalTo("Hello World")));
 	}
 
 }
