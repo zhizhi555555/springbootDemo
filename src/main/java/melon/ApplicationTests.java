04608c4a1703337218df6c0a1fd70a1e9df11c05
zhuxuguang555@163.com <zhuxuguang555@163.com>
2017-03-04 16:23:38 +0800

    springboot connection redis

---
+++ src/main/java/melon/ApplicationTests.java
@@ -0,0 +1,28 @@
+package melon;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(FirstApplication.class)
+public class ApplicationTests {
+
+	@Autowired
+	private StringRedisTemplate stringRedisTemplate;
+
+	@Test
+	public void test() throws Exception {
+
+		// 保存字符串
+		stringRedisTemplate.opsForValue().set("aaa", "111");
+		System.out.println(stringRedisTemplate.opsForValue().get("aaa"));
+		Assert.assertEquals("111", stringRedisTemplate.opsForValue().get("aaa"));
+
+    }
+
+}
--- src/main/test/melon/ApplicationTest.java
+++ src/main/test/melon/ApplicationTest.java
@@ -1,59 +1,49 @@
-package melon;
-
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
-
-
-
-/**
- *
- * @author 程序猿DD
- * @version 1.0.0
- * @blog http://blog.didispace.com
- *
- */
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(FirstApplication.class)
-public class ApplicationTest {
-
-	@Autowired
-	@Qualifier("primaryJdbcTemplate")
-	protected JdbcTemplate jdbcTemplate1;
-
-	@Autowired
-	@Qualifier("secondaryJdbcTemplate")
-	protected JdbcTemplate jdbcTemplate2;
-
-	@Before
-	public void setUp() {
-		jdbcTemplate1.update("DELETE  FROM  USER ");
-		jdbcTemplate2.update("DELETE  FROM  USER ");
-	}
-
-	@Test
-	public void test() throws Exception {
-
-		// 往第一个数据源中插入两条数据
-		jdbcTemplate1.update("insert into user(id,name,age) values(?, ?, ?)", 1, "aaa", 20);
-		jdbcTemplate1.update("insert into user(id,name,age) values(?, ?, ?)", 2, "bbb", 30);
-
-		// 往第二个数据源中插入一条数据，若插入的是第一个数据源，则会主键冲突报错
-		jdbcTemplate2.update("insert into user(id,name,age) values(?, ?, ?)", 1, "aaa", 20);
-
-		// 查一下第一个数据源中是否有两条数据，验证插入是否成功
-		Assert.assertEquals("2", jdbcTemplate1.queryForObject("select count(1) from user", String.class));
-
-		// 查一下第一个数据源中是否有两条数据，验证插入是否成功
-		Assert.assertEquals("1", jdbcTemplate2.queryForObject("select count(1) from user", String.class));
-
-	}
-
-}
+//package melon;
+//
+//
+//import melon.domain.User;
+//
+//import org.junit.Assert;
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.SpringApplicationConfiguration;
+//import org.springframework.data.redis.core.RedisTemplate;
+//import org.springframework.data.redis.core.StringRedisTemplate;
+//import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+//
+//
+//@RunWith(SpringJUnit4ClassRunner.class)
+//@SpringApplicationConfiguration(FirstApplication.class)
+//public class ApplicationTest{
+//
+//	@Autowired
+//	private StringRedisTemplate stringRedisTemplate;
+////
+//	@Autowired
+//	private RedisTemplate<String, User> redisTemplate;
+//
+//	@Test
+//	public void test() throws Exception {
+//
+////		// 保存字符串
+////		stringRedisTemplate.opsForValue().set("aaa", "111");
+////		Assert.assertEquals("111", stringRedisTemplate.opsForValue().get("aaa"));
+//
+//		// 保存对象
+//		User user = new User("超人", 20);
+//		redisTemplate.opsForValue().set(user.getUsername(), user);
+//
+//		user = new User("蝙蝠侠", 30);
+//		redisTemplate.opsForValue().set(user.getUsername(), user);
+//
+//		user = new User("蜘蛛侠", 40);
+//		redisTemplate.opsForValue().set(user.getUsername(), user);
+//
+//		Assert.assertEquals(20, redisTemplate.opsForValue().get("超人").getAge().longValue());
+//		Assert.assertEquals(30, redisTemplate.opsForValue().get("蝙蝠侠").getAge().longValue());
+//		Assert.assertEquals(40, redisTemplate.opsForValue().get("蜘蛛侠").getAge().longValue());
+//
+//	}
+//
+//}
